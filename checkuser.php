23fa8d059c63dbc054052aab3b755aecc8262a23
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-10-30 16:37:04 +0800

    Update

--- checkuser.php
+++ checkuser.php
@@ -1,12 +1,13 @@
 <?php include "database.php";
 
 $username = $_POST['username'];
+$password = $_POST['password'];
 
 $query = "SELECT * FROM user";
 $result = mysqli_query($conn,$query);
 $row = mysqli_fetch_assoc($result);
 
-if ($row['username'] == $username) {
+if ($row['username'] == $username && $row['password'] == $password) {
     echo "Successfully Login";
 }else {
     echo "Login Failed";
