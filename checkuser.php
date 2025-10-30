46f12aa5e92ffe2e38e92295f2c89ed0f74dfef4
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-10-30 16:54:29 +0800

    Add while loop

--- checkuser.php
+++ checkuser.php
@@ -5,11 +5,14 @@ $password = $_POST['password'];
 
 $query = "SELECT * FROM user";
 $result = mysqli_query($conn,$query);
-$row = mysqli_fetch_assoc($result);
+while($row = mysqli_fetch_assoc($result)) {
 
-if ($row['username'] == $username && $row['password'] == $password) {
-    echo "Successfully Login";
-}else {
-    echo "Login Failed";
+    if ($row['username'] == $username && $row['password'] == $password) {
+        echo "Successfully Login";
+    }else {
+        echo "Login Failed";
+    }
+
 }
+
 ?>
