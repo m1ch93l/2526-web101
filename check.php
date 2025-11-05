2b78c0e82ebbe2da7d81e1c0b4fab2b74d1a16f7
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2025-11-05 20:01:14 +0800

    Refactor user authentication logic for clarity and security

--- check.php
+++ check.php
@@ -7,15 +7,26 @@ $databasename = "studentdb";
 $conn         = new mysqli($servername, $username, $password, $databasename);
 
 $username = $_POST['un'];
-$password = $_POST['pw'];
+$input_password = $_POST['pw'];
 
 $query  = "SELECT * FROM user WHERE username = '$username' ";
 $result = mysqli_query($conn, $query);
 
-while ($row = mysqli_fetch_array($result)) {
-    if ($row['password'] == $password) {
+// Check if a user was found
+if (mysqli_num_rows($result) == 1) {
+    $row = mysqli_fetch_assoc($result); // Use fetch_assoc for clearer column access
+
+    if ($row['password'] == $input_password) {
+        // Success
         header('location: home.php');
+        exit;
     } else {
+        // Incorrect Password
         header('location: index.php');
+        exit;
     }
+} else {
+    // Incorrect Username (No rows found)
+    header('location: index.php');
+    exit;
 }
